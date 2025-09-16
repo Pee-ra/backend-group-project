ea82e16fdc6b049248185a9e48cabe5433a74369
watcharap <watchara.paramee@gmail.com>
2025-09-16 15:27:56 +0700

    add payment page data

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { createOrder, getMyOrders } from "../controllers/orderController.js";
+import { createOrder, getMyOrders, getOrderById } from "../controllers/orderController.js";
 import { protect } from "../middleware/auth.js";
+ 
 
 const router = express.Router();
 
 // ✅ ใช้ protect เพื่อเชื่อมกับ user จาก token
 router.post("/", protect, createOrder);
 router.get("/me", protect, getMyOrders);
+router.get("/:id", protect, getOrderById);
 
 export default router;
 
